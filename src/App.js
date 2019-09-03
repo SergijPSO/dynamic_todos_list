bfb48e58ab2fbe08eafe632b285a5f8459f9a8e5
SergijPSO <pankivsergij97@gmail.com>
2019-09-03 15:15:29 +0300

    add app structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 
+import todos from './todos';
+import users from './users';
+import TodoList from './TodoList';
+
 function App() {
   return (
     <div className="App">
       <h1>Dynamic list of todos</h1>
+      <TodoList items={todos} users ={users} />
     </div>
   );
 }
